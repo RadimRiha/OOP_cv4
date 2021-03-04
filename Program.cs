a7c44ccec19367763a5913f08739b4c115b98a14
RadimRiha <radim.rihacek@seznam.cz>
2021-03-04 14:58:27 +0100

    Finished all methods

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace OOP_cv4
     {
         static void Main(string[] args)
         {
-            string TestText = "Toto je retezec predstavovany nekolika radky,\n"
+            string TestText = "Toto je retezec , predstavovany nekolika radky,\n"
                                  + "ktere jsou od sebe oddeleny znakem LF (Line Feed).\n"
                                  + "Je tu i nejaky ten vykricnik! Pro ucely testovani i otaznik?\n"
                                  + "Toto je jen zkratka zkr. ale ne konec vety. A toto je\n"
@@ -24,6 +24,8 @@ namespace OOP_cv4
             Console.WriteLine("Longest words: {0}\n", string.Join(", ", Statistics.LongestWords()));
             Console.WriteLine("Shortest words: {0}\n", string.Join(", ", Statistics.ShortestWords()));
             Console.WriteLine("Most common words: {0}\n", string.Join(", ", Statistics.MostCommonWords()));
+            Console.WriteLine("Words in alphabetical order: {0}\n", string.Join(", ", Statistics.AlphabeticalOrder()));
+            Console.WriteLine("Is string infected: {0}\n", Statistics.IsInfected());
         }
     }
 }
--- StringStatistics.cs
+++ StringStatistics.cs
@@ -8,31 +8,19 @@ namespace OOP_cv4
 {
     class StringStatistics
     {
-        private string data = "";
+        private string data;
         
-        private static char[] wordSeparators = { ' ', '\n' };
+        private static char[] wordSeparators = { ' ', ',', '\n' };
         private static char[] punctuation = { '.', ',', ':', ';', '?', '!', '(', ')' };
         private static char[] sentenceEndings = { '.', '?', '!' };
+        private static string[] infectedWords = { "covid", "covid-19", "sars-cov-2" };
 
         public StringStatistics(string dataIn)
         {
+            if (dataIn.Length == 0) throw new Exception("Cannot construct string statistics with no input");
             data = dataIn;
         }
-        private static string removeDuplicate(string dataIn, char[] letters)
-        {
-            string result = "";
-            char prevLetter = dataIn[0];
-            foreach (char letter in dataIn)
-            {
-                if (!(letters.Contains(letter) && letters.Contains(prevLetter)))
-                {
-                    result += letter;
-                }
-                prevLetter = letter;
-            }
-            return result;
-        }
-        private static string removeAny(string dataIn, char[] letters)
+        private string removeAny(string dataIn, char[] letters)    //removes specified letters from input string
         {
             string result = dataIn;
             foreach (char letter in letters)
@@ -41,9 +29,13 @@ namespace OOP_cv4
             }
             return result;
         }
+        private string[] splitWords()
+        {
+            return removeAny(data, punctuation).Split(wordSeparators, StringSplitOptions.RemoveEmptyEntries);
+        }
         public int WordCount()
         {
-            return data.Split(wordSeparators).Length;
+            return splitWords().Length;
         }
         public int LineCount()
         {
@@ -51,10 +43,11 @@ namespace OOP_cv4
         }
         public int SentenceCount()
         {
-            string[] stringSplit = removeDuplicate(data, wordSeparators).Split(wordSeparators);
+            string[] stringSplit = data.Split(wordSeparators, StringSplitOptions.RemoveEmptyEntries);
             int result = 1;
             for (int wordIndex = 0; wordIndex < stringSplit.Length-1; wordIndex++)
             {
+                //new sentence is detected as a sentence ending character followed by an upper case letter
                 if (sentenceEndings.Contains(stringSplit[wordIndex][stringSplit[wordIndex].Length - 1]) && Char.IsUpper(stringSplit[wordIndex + 1][0]))
                 {
                     result++;
@@ -64,10 +57,10 @@ namespace OOP_cv4
         }
         public string[] LongestWords()
         {
-            string[] stringSplit = removeAny(removeDuplicate(data, wordSeparators), punctuation).Split(wordSeparators);
+            string[] stringSplit = splitWords();
             int maxLength = 0;
             int resultSize = 1;
-            foreach (string word in stringSplit)
+            foreach (string word in stringSplit)    //find length of longest words and amount of these words
             {
                 if (word.Length > maxLength)
                 {
@@ -78,7 +71,7 @@ namespace OOP_cv4
             }
             string[] result = new string[resultSize];
             int resultIndex = 0;
-            foreach (string word in stringSplit)
+            foreach (string word in stringSplit)    //load result array with longest words
             {
                 if (word.Length == maxLength)
                 {
@@ -90,10 +83,10 @@ namespace OOP_cv4
         }
         public string[] ShortestWords()
         {
-            string[] stringSplit = removeAny(removeDuplicate(data, wordSeparators), punctuation).Split(wordSeparators);
+            string[] stringSplit = splitWords();
             int minLength = stringSplit[0].Length;
             int resultSize = 1;
-            foreach (string word in stringSplit)
+            foreach (string word in stringSplit)    //find length of shortest words and amount of these words
             {
                 if (word.Length < minLength)
                 {
@@ -104,7 +97,7 @@ namespace OOP_cv4
             }
             string[] result = new string[resultSize];
             int resultIndex = 0;
-            foreach (string word in stringSplit)
+            foreach (string word in stringSplit)    //load result array with shortest words
             {
                 if (word.Length == minLength)
                 {
@@ -116,26 +109,26 @@ namespace OOP_cv4
         }
         public string[] MostCommonWords()
         {
-            string[] stringSplit = removeAny(removeDuplicate(data, wordSeparators), punctuation).Split(wordSeparators);
-            string uniqueWords = " ";
+            string[] stringSplit = splitWords();
+            string uniqueWordString = " ";
             int uniqueWordsCount = 0;
-            foreach (string word in stringSplit)
+            foreach (string word in stringSplit)    //convert string array into a string of unique words
             {
-                if (!uniqueWords.Contains(" " + word + " "))
+                if (!uniqueWordString.Contains(" " + word + " "))
                 {
                     uniqueWordsCount++;
-                    uniqueWords += word + " ";
+                    uniqueWordString += word + " ";
                 }
             }
             int[] wordCounts = new int[uniqueWordsCount];
-            string[] words = uniqueWords.Substring(1,uniqueWords.Length-2).Split(' ');
-            foreach (string word in stringSplit)
+            string[] uniqueWords = uniqueWordString.Substring(1, uniqueWordString.Length-2).Split(' ');
+            foreach (string word in stringSplit)    //count the amount of each unique word in the original data
             {
-                wordCounts[Array.IndexOf(words, word)]++;
+                wordCounts[Array.IndexOf(uniqueWords, word)]++;
             }
             int maxCount = 0;
             int resultSize = 1;
-            for (int wordIndex = 0; wordIndex < wordCounts.Length; wordIndex++)
+            for (int wordIndex = 0; wordIndex < wordCounts.Length; wordIndex++)     //find the most common words and amount of these words
             {
                 if (wordCounts[wordIndex] > maxCount)
                 {
@@ -146,15 +139,30 @@ namespace OOP_cv4
             }
             string[] result = new string[resultSize];
             int resultIndex = 0;
-            for (int countsIndex = 0; countsIndex < wordCounts.Length; countsIndex++)
+            for (int wordIndex = 0; wordIndex < wordCounts.Length; wordIndex++)     //load result array with shortest words
             {
-                if (wordCounts[countsIndex] == maxCount)
+                if (wordCounts[wordIndex] == maxCount)
                 {
-                    result[resultIndex] = words[countsIndex];
+                    result[resultIndex] = uniqueWords[wordIndex];
                     resultIndex++;
                 }
             }
             return result;
+        }
+        public string[] AlphabeticalOrder()
+        {
+            string[] stringSplit = splitWords();
+            Array.Sort(stringSplit);
+            return stringSplit;
+        }
+        public bool IsInfected()
+        {
+            string[] stringSplit = splitWords();
+            foreach (string word in stringSplit)
+            {
+                if (infectedWords.Contains(word.ToLower())) return true;
+            }
+            return false;
         }
     }
 }
